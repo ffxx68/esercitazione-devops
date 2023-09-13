2fd19c251cf3459bfadc36336022b69106ff60da
ffumi <f.fumi@almaviva.it>
2023-09-13 16:05:23 +0200

    calcolatrice

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-# my python script
+# esercizio 2
+from operazioni import * 
+
+while True:
+    oper = input ("operazione (+, -, *, /, x=fine) ")
+    if oper != "x":
+        if oper == "+" or oper == "-" or oper == "*" or oper == "/":
+            num1 = input ("numero 1 ")
+            num2 = input ("numero 2 ")
+            if num1.isnumeric() and num2.isnumeric():
+                if oper == "-":
+                    risul = diff (float(num1), float(num2))
+                elif oper == "+":
+                    risul = somma (float(num1), float(num2))
+                elif oper == "*":
+                    risul = moltip(float(num1), float(num2))
+                elif oper == "/":
+                    risul = divis(float(num1), float(num2))
+                else :
+                    pass
+            else:
+                print ("valori non numerici")
+                quit()
+        else :
+            print ("operazione non ammessa")
+            quit()
+    else:
+        quit()
+
+    print ("risultato: "+str(risul))
+
---
+++ operazioni.py
@@ -0,0 +1,11 @@
+def diff(x, y):
+    return x-y
+
+def somma(x, y):
+    return x+y
+
+def moltip(x, y):
+    return x*y
+
+def divis(x, y):
+    return x/y
